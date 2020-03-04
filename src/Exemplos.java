5b6fa6825bef87a97bef79d36e4b35af9b39218a
samuelchdias <samuelchdias@gmail.com>
2020-03-04 20:02:07 -0300

    comentario inicial

---
+++ src/Exemplos.java
@@ -0,0 +1,14 @@
+
+public class Exemplos {
+	public static void main(String[] args) {
+		int idade = 17;
+		if (idade >= 18)
+			System.out.println("maior de idade");
+		} else {
+			System.out.println("menor de idade");
+
+		}
+		
+	}
+
+}
